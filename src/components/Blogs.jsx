08fcc60c7dbff0ba18472bcb7939674666b0c96d
ArshnoorSingh07 <asingh39_be23@thapar.edu>
2026-01-09 20:54:38 +0530

    Update Blogs.jsx

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
 import Spinner from './Spinner';
+import BlogDetails from './BlogDetails';
 
 const Blogs = () => {
 
@@ -22,27 +23,7 @@ const Blogs = () => {
               ) 
             : (
                 posts.map((post)=>(
-                  <div key={post.id}>
-                    <p className='font-bold text-lg'>{post.title}</p>
-
-                    <p className='text-sm'>
-                      By <span className='italic'>{post.author}</span> on <span className='underline font-bold'>{post.category}</span>
-                    </p>
-                    
-                    <p className='text-sm mt-[4px]'>
-                      Posted on {post.date}
-                    </p>
-
-                    <p className='text-md mt-[14px]'>
-                      {post.content}
-                    </p>
-
-                    <div className='flex gap-x-3'>
-                      {post.tags.map((tag,index)=>{
-                        return <span className='text-blue-700 font-bold underline text-xs mt-[5px]' key={index}>{`#${tag}`}</span>
-                      })}
-                    </div>
-                  </div>
+                  <BlogDetails key={post.id} post={post}/>
                 ))  
                )
             )
