9520da6435caea384906198fc5540c9928e04163
ArshnoorSingh07 <asingh39_be23@thapar.edu>
2026-01-09 20:54:41 +0530

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,5 +1,6 @@
 import { createContext,  useState } from "react";
 import { baseUrl } from "../baseUrl";
+import { useNavigate } from "react-router-dom";
 
 // step 1
 export const AppContext=createContext();
@@ -10,24 +11,28 @@ export default function AppContextProvider({children})
     const [posts,setPosts]=useState([]);
     const [page,setPage]=useState(1);
     const [totalPages,setTotalPages]=useState(null);
+    const navigate=useNavigate();
 
     // data fill
-    async function fetchBlogPost(page=1 ){
+    async function fetchBlogPost(page=1,tag=null,category){
         setLoading(true);
-        let url=`${baseUrl}?page=${page}`
-        console.log("Printing The base URL: ");
-        console.log(url);
-
+        let url=`${baseUrl}?page=${page}`;
+        if(tag){
+            url+=`&tag=${tag}`
+        }
+        if(category){
+            url+=`&category=${category}`
+        }
         try{
             const response=await fetch(url);
             const data=await response.json();
-            console.log(data);
+            console.log("API response: ",data);
             setPage(data.page);
             setPosts(data.posts);
             setTotalPages(data.totalPages);
         }
         catch(error){
-            console.log("Error in fetching data");
+            console.log("Error in fetching BlogPosts");
             setPage(1);
             setPosts([]);
             setTotalPages(null);
@@ -38,7 +43,8 @@ export default function AppContextProvider({children})
 
     function handlePageChange(page){
         setPage(page);
-        fetchBlogPost(page);
+        navigate({search: `page=${page}`});
+         
     }
 
 
